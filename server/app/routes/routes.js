6caccfb3e41b1acfb6f3554d21076419220b9e30
ysera34 <ycyoon34@gmail.com>
2018-05-28 00:02:19 +0900

    add video route, config static files

--- server/app/routes/routes.js
+++ server/app/routes/routes.js
@@ -11,4 +11,21 @@ module.exports = function(app) {
   app.get('/switch', (req, res) => {
     res.send('hello get');
   });
+
+  app.get('/videos/:id', (req, res) => {
+    const id = req.params.id;
+
+    switch (id) {
+      case '1':
+          console.log('id: ' + id);
+          res.render('video11.pug');
+        break;
+      case '2':
+          console.log('id: ' + id);
+          res.render('video12.pug');
+        break;
+      default:
+        res.render('index');
+    }
+  });
 };
--- server/server.js
+++ server/server.js
@@ -5,12 +5,16 @@ const path = require('path');
 const app = express();
 const port = 3001;
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 require('./app/routes')(app, {});
+// const router = express.Router();
+// const routes = require('./app/routes')(router, {});
+// app.use('/', routes);
 
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
 
 app.listen(port, () => {
   console.log('server is running on ' + port);
